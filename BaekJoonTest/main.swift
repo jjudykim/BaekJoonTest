781eb0c47be60c5be8c08f61ae9ea5d131eb3e31
jjudykim <imjubaaakim@gmail.com>
2022-07-23 03:29:26 +0900

    2739 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,31 +5,11 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 2480
+// 2739
 import Foundation
 
-let line = readLine()!
-let diceArr = line.components(separatedBy: " ")
-let diceA = Int(diceArr[0])!
-let diceB = Int(diceArr[1])!
-let diceC = Int(diceArr[2])!
+let n = Int(readLine()!)!
 
-var score = 0
-var maxDice = 0
-//같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
-//같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
-//모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
-
-if (diceA == diceB) && (diceB == diceC) {
-    score = 10000 + (diceA * 1000)
-} else if (diceA == diceB) || (diceA == diceC) {
-    score = 1000 + (diceA * 100)
-} else if (diceB == diceC) {
-    score = 1000 + (diceB * 100)
-} else {
-    maxDice = diceA >= diceB ? diceA : diceB
-    maxDice = diceC >= maxDice ? diceC : maxDice
-    score = maxDice * 100
+for i in 1...9 {
+    print("\(n) * \(i) = \(n*i)")
 }
-
-print(score)
