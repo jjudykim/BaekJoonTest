25ca1dfa0dd4cecbcb838d5ee5cfd5a3877afa5e
jjudykim <imjubaaakim@gmail.com>
2022-07-22 23:15:50 +0900

    2884 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,7 +5,29 @@
 //  Created by jjudy on 2022/07/22.
 //
 
+// 2884
 import Foundation
 
-print("Hello, World!")
+let line = readLine()!
+let timeArr = line.components(separatedBy: " ")
+let hour = Int(timeArr[0])!
+let min = Int(timeArr[1])!
+
+var changeH = hour
+var changeM = min
+    
+if min - 45 < 0 {
+    if changeH == 0 {
+        changeH = 23
+    } else {
+        changeH -= 1
+    }
+    changeM = 60 + (changeM - 45)
+} else {
+    changeM -= 45
+}
+
+print(changeH, terminator: " ")
+print(changeM)
+
 
