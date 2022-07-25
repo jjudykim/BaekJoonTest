ec6d422d85b17fdf131d0515300098355a88fd8c
jjudykim <imjubaaakim@gmail.com>
2022-07-25 23:45:31 +0900

    10950 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,11 +5,20 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 2739
+// 10950
 import Foundation
 
-let n = Int(readLine()!)!
+let num = Int(readLine()!)!
+var nums: [(Int, Int)] = []
 
-for i in 1...9 {
-    print("\(n) * \(i) = \(n*i)")
+for i in 1...num {
+    let readNum = readLine()!
+    let readNumArr = readNum.components(separatedBy: " ")
+    let firstNum = Int(readNumArr[0])!
+    let secondNum = Int(readNumArr[1])!
+    nums.append((firstNum, secondNum))
+}
+
+for i in nums {
+    print(i.0 + i.1)
 }
