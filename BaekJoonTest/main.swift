6fad17c551cef00bfdd8cda23f749d9fc08e17e2
jjudy_kim / 김주향 <66450769+jjudykim@users.noreply.github.com>
2022-07-27 00:13:45 +0900

    11021 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,11 +5,19 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 2742
+// 11021
 import Foundation
 
 var num = Int(readLine()!)!
+var numArray: Array<(Int, Int)> = []
 
-for i in (1...num).reversed() {
-    print(i)
+for _ in 1...num {
+    let readArray = readLine()!.components(separatedBy: " ")
+    let firstNum = Int(readArray[0])!
+    let secondNum = Int(readArray[1])!
+    numArray.append((firstNum, secondNum))
+}
+
+for i in 0...numArray.count-1{
+    print("Case #\(i+1): \(numArray[i].0 + numArray[i].1)")
 }
