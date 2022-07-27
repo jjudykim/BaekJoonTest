77e5828364f8fc116d52c967b760c78dce87b4e9
jjudy_kim <jjudykim0@gmail.com>
2022-07-27 13:31:14 +0900

    2439 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,13 +5,17 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 2438
-// 문제 제대로 읽기
+// 2439
 import Foundation
 
 let num = Int(readLine()!)!
 
 for i in 1...num{
+    if(num - i != 0) {
+        for _ in 1...num - i {
+            print(" ", terminator: "")
+        }
+    }
     for _ in 1...i {
         print("*", terminator: "")
     }
