734bd1c5460e7b34e78085778fc4b31e64c7e574
jjudy_kim <jjudykim0@gmail.com>
2022-07-28 23:24:03 +0900

    1110 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,20 +5,22 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 10951
+// 1110
 import Foundation
 
-var resultArr: Array<Int> = []
+var cycle = 0
+
+var num = Int(readLine()!)!
+let numOfGoal = num
 
 while true {
-    guard let readInput = readLine() else { break }
-    let inputArr = readInput.components(separatedBy: " ")
-    guard let firstNum = Int(inputArr[0]) else { break }
-    guard let secondNum = Int(inputArr[1]) else { break }
-    resultArr.append(firstNum + secondNum)
+    cycle += 1
+    let firstNum = num/10
+    let secondNum = num%10
+    let sumOfNum = firstNum + secondNum
+    let newNum = secondNum * 10 + sumOfNum % 10
+    num = newNum
+    if newNum == numOfGoal { break }
 }
 
-for i in resultArr {
-    print(i)
-}
-
+print(cycle)
