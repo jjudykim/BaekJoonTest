8f08bf7ca13964e5d97d58e00f91c1d5fdda49ec
jjudy_kim <jjudykim0@gmail.com>
2022-07-28 23:13:50 +0900

    10951 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,17 +5,16 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 10952
+// 10951
 import Foundation
 
 var resultArr: Array<Int> = []
 
 while true {
-    let inputArr = readLine()!.components(separatedBy: " ")
-    let firstNum = Int(inputArr[0])!
-    let secondNum = Int(inputArr[1])!
-    if firstNum == 0 && secondNum == 0 { break }
-    
+    guard let readInput = readLine() else { break }
+    let inputArr = readInput.components(separatedBy: " ")
+    guard let firstNum = Int(inputArr[0]) else { break }
+    guard let secondNum = Int(inputArr[1]) else { break }
     resultArr.append(firstNum + secondNum)
 }
 
