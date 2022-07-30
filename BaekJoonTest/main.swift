5b1eb58f0cd7e567bbf3dbbcb9101a4d7f0cc56e
jjudy_kim <jjudykim0@gmail.com>
2022-07-31 00:04:11 +0900

    3052 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,27 +5,15 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 2577
+// 3052
 import Foundation
 
 var numArr: Array<Int> = []
-var sumNum = 0
-var strNum = ""
 
-for _ in 1...3 {
+for _ in 1...10 {
     let readNum = Int(readLine()!)!
-    numArr.append(readNum)
+    guard !(numArr.contains(readNum % 42)) else { continue }
+    numArr.append(readNum % 42)
 }
 
-sumNum = numArr[0] * numArr[1] * numArr[2]
-strNum = String(sumNum)
-
-for i in 0...9 {
-    var count = 0
-    for j in strNum {
-        if String(i) == String(j) {
-            count += 1
-        }
-    }
-    print(count)
-}
+print(numArr.count)
