49ec5f274e3f5ac1766873db1d8e80dc9eb5eb46
jjudy_kim <jjudykim0@gmail.com>
2022-08-02 01:09:48 +0900

    11654 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,23 +5,8 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 4344
+// 11654
 import Foundation
 
-let testCount = Int(readLine()!)!
-
-for _ in 1...testCount {
-    let scoreArr = readLine()!.split(separator: " ").map { Int(String($0))! }
-    var sumScore = 0
-    var avgOverCount = 0
-    for i in scoreArr[1...] {
-        sumScore += i
-    }
-    let avgScore = Double(sumScore) / Double(scoreArr[0])
-    
-    for i in scoreArr[1...] {
-        if Double(i) > avgScore { avgOverCount += 1 }
-    }
-    
-    print( String(format: "%.3f", Double(avgOverCount) / Double(scoreArr.count - 1) * 100) + "%" )
-}
+let readStr = Character(readLine()!)
+print(readStr.asciiValue!)
