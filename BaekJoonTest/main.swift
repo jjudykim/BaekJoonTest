dd0a7b4f02b4b1f53f9578cad6252bceb1e01c12
jjudy_kim <jjudykim0@gmail.com>
2022-08-07 23:34:09 +0900

    1037 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -5,15 +5,23 @@
 //  Created by jjudy on 2022/07/22.
 //
 
-// 3003
-// 체스는 총 16개의 피스를 사용하며, 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개로 구성되어 있다.
-// 첫째 줄에 동혁이가 찾은 흰색 킹, 퀸, 룩, 비숍, 나이트, 폰의 개수가 주어진다. 이 값은 0보다 크거나 같고 10보다 작거나 같은 정수이다.
-// 첫째 줄에 입력에서 주어진 순서대로 몇 개의 피스를 더하거나 빼야 되는지를 출력한다. 만약 수가 양수라면 동혁이는 그 개수 만큼 피스를 더해야 하는 것이고, 음수라면 제거해야 하는 것이다.
+// 1037
+// 양수 A가 N의 진짜 약수가 되려면, N이 A의 배수이고, A가 1과 N이 아니어야 한다. 어떤 수 N의 진짜 약수가 모두 주어질 때, N을 구하는 프로그램을 작성하시오.
+// 첫째 줄에 N의 진짜 약수의 개수가 주어진다. 이 개수는 50보다 작거나 같은 자연수이다.
+// 둘째 줄에는 N의 진짜 약수가 주어진다. 1,000,000보다 작거나 같고, 2보다 크거나 같은 자연수이고, 중복되지 않는다.
 import Foundation
 
-let inputArr = readLine()!.components(separatedBy: " ")
-let correctArr = [1, 1, 2, 2, 2, 8]
+let countOfNum = Int(readLine()!)!
+var numArr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var result = 0
 
-for i in 0...inputArr.count-1 {
-    print(correctArr[i] - Int(inputArr[i])!, terminator: " ")
+numArr.sort()
+
+if numArr.count % 2 == 0 {
+    result = numArr[0] * numArr[numArr.count - 1]
+} else {
+    let index = (numArr.count - 1) / 2
+    result = numArr[index] * numArr[index]
 }
+
+print(result)
