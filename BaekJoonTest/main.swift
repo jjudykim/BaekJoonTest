30c26daff5e10b5c7d266f9a2daf3c150735be27
jjudy_kim <jjudykim0@gmail.com>
2022-08-11 00:13:10 +0900

    1152 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -7,23 +7,22 @@
 
 import Foundation
 
-// 25304
-// 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하는지 검사해보자.
-// 첫째 줄에는 영수증에 적힌 총 금액 X가 주어진다.
-// 둘째 줄에는 영수증에 적힌 구매한 물건의 종류의 수 N이 주어진다.
-// 이후 N개의 줄에는 각 물건의 가격 a와 개수 b가 공백을 사이에 두고 주어진다.
+// 1152
+// 영어 대소문자와 공백으로 이루어진 문자열이 주어진다.
+// 이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오.
+// 단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
 
 func mine() {
-    let total = Int(readLine()!)!
-    let count = Int(readLine()!)!
-    var listTotal = 0
+    let str = readLine()!.components(separatedBy: " ")
+    var countStr = str.count
     
-    for _ in 1...count {
-        let list = readLine()!.split{ $0 == " " }.map{ Int($0)! }
-        listTotal += (list[0] * list[1])
+    for i in str {
+        if i == "" {
+            countStr -= 1
+        }
     }
     
-    total == listTotal ? print("Yes") : print("No")
+    print(countStr)
 }
 
 mine()
