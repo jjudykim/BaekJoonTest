143535f755028f804650e6446813d514f7947346
jjudy_kim <jjudykim0@gmail.com>
2022-08-11 00:41:30 +0900

    2908 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -4,29 +4,20 @@
 //
 //  Created by jjudy on 2022/07/22.
 //
+// 2908
+// 예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다. 따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
+// 두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
 
 import Foundation
 
-// 1152
-// 영어 대소문자와 공백으로 이루어진 문자열이 주어진다.
-// 이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오.
-// 단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
+let str = readLine()!.components(separatedBy: " ")
+var num1 = Array(str[0])
+var num2 = Array(str[1])
 
-func mine() {
-    let str = readLine()!.components(separatedBy: " ")
-    var countStr = str.count
-    
-    for i in str {
-        if i == "" {
-            countStr -= 1
-        }
-    }
-    
-    print(countStr)
-}
+num1.swapAt(0, 2)
+num2.swapAt(0, 2)
 
-mine()
+let numInt1 = Int(String(num1))!
+let numInt2 = Int(String(num2))!
 
-
-
-
+numInt1 > numInt2 ? print(numInt1) : print(numInt2)
