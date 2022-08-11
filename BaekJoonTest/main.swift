4550acc6f8802f34047e7e6d1140c20a53024a7f
jjudy_kim <jjudykim0@gmail.com>
2022-08-12 00:14:16 +0900

    5622 done.

--- BaekJoonTest/main.swift
+++ BaekJoonTest/main.swift
@@ -4,25 +4,42 @@
 //
 //  Created by jjudy on 2022/07/22.
 //
-// 2908
-// 예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다. 따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
-// 두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
+// 5622
+// 숫자 1을 걸려면 총 2초가 필요하다.
+// 1보다 큰 수를 거는데 걸리는 시간은 이보다 더 걸리며, 한 칸 옆에 있는 숫자를 걸기 위해선 1초씩 더 걸린다.
+// 상근이의 할머니는 전화 번호를 각 숫자에 해당하는 문자로 외운다.
+// 즉, 어떤 단어를 걸 때, 각 알파벳에 해당하는 숫자를 걸면 된다. 예를 들어, UNUCIC는 868242와 같다.
+
 
 import Foundation
 
-let str = readLine()!.components(separatedBy: " ")
-var num1 = Array(str[0])
-var num2 = Array(str[1])
-
-num1.swapAt(0, 2)
-num2.swapAt(0, 2)
-
-let numInt1 = Int(String(num1))!
-let numInt2 = Int(String(num2))!
-
-numInt1 > numInt2 ? print(numInt1) : print(numInt2)
+let inputStr = Array(readLine()!)
+var countTime = 0
 
 
-// 1등 코드
-var input = readLine()!.split(separator: " ").map { String($0.reversed()) }   // 역순으로 매핑
-print(input.max()!)   // 가장 큰 값 출력
+for i in inputStr {
+    var temp = 0
+    switch i {
+    case "A", "B", "C" :
+        temp = 2
+    case "D", "E", "F" :
+        temp = 3
+    case "G", "H", "I" :
+        temp = 4
+    case "J", "K", "L" :
+        temp = 5
+    case "M", "N", "O" :
+        temp = 6
+    case "P", "Q", "R", "S" :
+        temp = 7
+    case "T", "U", "V" :
+        temp = 8
+    case "W", "X", "Y", "Z" :
+        temp = 9
+    default:
+        break
+    }
+    countTime += 2 + temp - 1
+}
+
+print(countTime)
